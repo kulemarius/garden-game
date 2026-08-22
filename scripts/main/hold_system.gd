843628b9e6675657ed36ce18cca8be3bc485aa87
marzz <magoos69420@gmail.com>
2026-08-22 12:52:46 +0200

    added scripts for camera zoom

--- scripts/main/hold_system.gd
+++ scripts/main/hold_system.gd
@@ -1,7 +1,68 @@
 extends Node
 
-@onready var Hand = $Player/Hand
+@onready var player = $"../Player"
+@onready var hand = $"../Player/Hand"
+@onready var pickup_area = $"../Player/PickupArea"
 
-var equippedItem
-var isSeed
-var canDrop
+var equippedItem = null
+var canDrop = true
+var nearbyItem = null
+
+func _ready():
+	pickup_area.area_entered.connect(_on_pickup_area_entered)
+	pickup_area.area_exited.connect(_on_pickup_area_exited)
+
+func _process(delta):
+	if equippedItem:
+		equippedItem.global_position = hand.global_position
+
+	if Input.is_action_just_pressed("Pickup"):
+		pickup()
+
+	if Input.is_action_just_pressed("Drop"):
+		drop()
+
+	if Input.is_action_just_pressed("Plant"):
+		plant()
+
+func _on_pickup_area_entered(area):
+	if area.is_in_group("Pickup"):
+		nearbyItem = area
+
+func _on_pickup_area_exited(area):
+	if area == nearbyItem:
+		nearbyItem = null
+
+func pickup():
+	if equippedItem:
+		return
+
+	if nearbyItem:
+		equippedItem = nearbyItem
+
+func drop():
+	if not equippedItem or not canDrop:
+		return
+
+	equippedItem.global_position = hand.global_position + Vector2(0, 30)
+	equippedItem = null
+
+func plant():
+	if not equippedItem:
+		return
+
+	if not "plant_scene" in equippedItem:
+		return
+
+	if equippedItem.plant_scene == null:
+		return
+
+	var plant_position = equippedItem.global_position
+	var plant_scene = equippedItem.plant_scene
+
+	equippedItem.queue_free()
+	equippedItem = null
+
+	var plant = plant_scene.instantiate()
+	get_tree().current_scene.add_child(plant)
+	plant.global_position = plant_position
