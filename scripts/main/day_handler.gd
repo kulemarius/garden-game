2133c85d6e3a2ccac2c72ecb4d5d9af7b4320a57
marzz <magoos69420@gmail.com>
2026-08-22 20:40:52 +0200

    added starts and changed day_handler

--- scripts/main/day_handler.gd
+++ scripts/main/day_handler.gd
@@ -1,8 +1,12 @@
 extends Node
 
+@export var day_color: Color = Color.WHITE
+@export var night_color: Color = Color(0.05, 0.05, 0.1)
+
 @onready var timer = $DayTimer
 @onready var label = $Canvas/UI/TimeleftLabel
 @onready var sprite = $Canvas/UI/Clock_Fill
+@onready var color_rect = $Background
 
 func _ready():
 	timer.timeout.connect(_on_timer_timeout)
@@ -11,7 +15,10 @@ func _process(_delta):
 	label.text = "%.2f" % timer.time_left
 	
 	var progress = 1.0 - (timer.time_left / 119.99)
+	
 	sprite.rotation = deg_to_rad(180.0 * progress)
+	
+	color_rect.color = day_color.lerp(night_color, progress)
 
 func _on_timer_timeout():
 	get_tree().reload_current_scene()
