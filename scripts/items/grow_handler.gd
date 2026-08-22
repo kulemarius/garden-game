50036c45b169f4173c605736e2fd9a30e40dc81c
marzz <magoos69420@gmail.com>
2026-08-22 14:22:33 +0200

    added ui and pixel shader

--- scripts/items/grow_handler.gd
+++ scripts/items/grow_handler.gd
@@ -1,32 +1,25 @@
 extends Node2D
 
 @export var grow_time := 5.0
-@export var time_to_die := 15.0
 @export var pickup_scene: PackedScene
 
 @onready var sprite = $AnimatedSprite2D
-@onready var grow_timer = $GrowTimer
-@onready var death_timer = $DeathTimer
+@onready var grow_timer = $Timer
+@onready var arrowSprite = $Sprite2D
 
 var current_stage := 1
 var can_harvest := false
-var dying := false
-var dead := false
 
 func _ready():
 	grow_timer.one_shot = true
-	death_timer.one_shot = true
-
 	grow_timer.timeout.connect(_on_grow_timer_timeout)
-	death_timer.timeout.connect(_on_death_timer_timeout)
 
 	sprite.play("Stage1")
 	grow_timer.start(grow_time)
+	
+	arrowSprite.visible = false
 
 func _on_grow_timer_timeout():
-	if dying or dead:
-		return
-
 	current_stage += 1
 
 	var next_animation = "Stage" + str(current_stage)
@@ -36,23 +29,8 @@ func _on_grow_timer_timeout():
 		grow_timer.start(grow_time)
 	else:
 		sprite.play("FinalStage")
+		arrowSprite.visible = true
 		can_harvest = true
-		death_timer.start(time_to_die)
-
-func _on_death_timer_timeout():
-	if not can_harvest:
-		return
-
-	can_harvest = false
-	dying = true
-
-	sprite.play("Dying")
-	sprite.animation_finished.connect(_on_dying_finished, CONNECT_ONE_SHOT)
-
-func _on_dying_finished():
-	dying = false
-	dead = true
-	sprite.play("Dead")
 
 func harvest():
 	if not can_harvest:
