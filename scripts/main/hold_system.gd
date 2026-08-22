1bd242fc3460029d5e408943e84bf89bd8b107cc
marzz <magoos69420@gmail.com>
2026-08-22 12:05:05 +0200

    made changes to hold_system

--- scripts/main/hold_system.gd
+++ scripts/main/hold_system.gd
@@ -1,11 +1,7 @@
 extends Node
 
+@onready var Hand = $Player/Hand
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var equippedItem
+var isSeed
+var canDrop
